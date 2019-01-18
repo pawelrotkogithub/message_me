407feb990ae14414b29166f62d4003ded26e99cb
pawelrotkogithub <pawel_rotko@gmail.com>
2019-01-18 19:28:47 +0100

    Create User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Pawel", password: "password")
+User.create(username: "Ozon", password: "password")
+User.create(username: "Murab", password: "password")
+User.create(username: "Beryb", password: "password")
