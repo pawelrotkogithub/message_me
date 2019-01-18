070fc15e19afe34578cc44cea5c978414449500b
pawelrotkogithub <pawel_rotko@gmail.com>
2019-01-18 14:30:34 +0100

    Add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown')
+  .dropdown()
+;
+})
